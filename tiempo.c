a510bbc4b44fa9399777854e21f3d401d659eb02
Jmonarca <jmonarca522@gmail.com>
2024-12-28 18:15:01 -0600

    se agrega el archivo tiempo.c , con las funciones para convertir segundos a horas, minutos, minutos a horas y segundos etc. Tambian se agrega la funcion para actualizar la pantalla llamada actualizar_pantalla.

--- tiempo.c
+++ tiempo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef char String[100];
 
@@ -9,14 +10,68 @@ struct S_fecha {
     int anio;
 };
 
-int conversor(int segundos,int horas, int operacion){
-    int minutos;
-    if(operacion==1){
-        minutos=segundos/60;
-        return minutos;
+int conv_seg_horas(int segundos,int horas, int operacion){
+    
+    switch (operacion)
+    {
+    case 1:
+        segundos=horas*60;
+        return segundos;//retorna las horas en segundos
+        break;
+    case 2:
+        horas=segundos/60;
+        return horas;//reetorna los segundos en horas
+        break;
+    default:
+    printf("Operacion no valida");//mensaje de errr
+        break;
     }
-    else{
-        minutos=horas*60;
-        return minutos;
-    }
+    
 }
+
+int conv_seg_min(int segundos,int minutos, int operacion){
+    
+    switch (operacion)
+    {
+    case 1:
+        segundos=minutos*60;
+        return segundos;//retorna los minutos en segundos
+        break;
+    case 2:
+        minutos=segundos/60;
+        return minutos;//retorna los segundos en minutos
+        break;
+    default:
+    printf("Operacion no valida");//mensaje de error
+        break;
+    }
+    
+}
+
+int conv_min_horas(int minutos,int horas, int operacion){
+    
+    switch (operacion)
+    {
+    case 1:
+        minutos=horas*60;
+        return minutos;//retorna las horas en minutos
+        break;
+    case 2:
+        horas=minutos/60;
+        return horas;//retorna los minutos en horas
+        break;
+    default:
+    printf("Operacion no valida");//mensaje de error
+        break;
+    }
+    
+}
+
+void frame( void (*draw)()){
+
+        system("cls");
+        draw();
+
+}//funciona para actualizar la pantalla
+
+
