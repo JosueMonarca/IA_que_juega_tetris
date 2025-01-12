5c42817a14a0eee4cbc7ba5c359becd757fc0e35
Jmonarca <jmonarca522@gmail.com>
2025-01-12 16:24:00 -0600

    se grega el palo de el terix con una dimension de 4x1

--- codigos/objetos.c
+++ codigos/objetos.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Se declara la tabla
 char tabla[22][22];
 
+//Se declara el cuadrado{
 int cuadrado[4][2];
 
 void coordenadas_de_cuadrado (int x, int y){
@@ -19,4 +21,38 @@ void coordenadas_de_cuadrado (int x, int y){
     cuadrado[3][0]=x+1;//cordenada y
     cuadrado[3][1]=y+1;//cordenada x
 
+}
+//}se termina le estructura del cuadrado
+
+//Se declara la estructura de el palo
+int palo[22][22];
+
+void coordenadas_de_el_palo(int x, int y, int rotacion){
+    int coordenada_x;
+    int coordenada_y;
+    
+    for(coordenada_y=0;coordenada_y<22;coordenada_y++){
+        for(coordenada_x=0;coordenada_x<22;coordenada_x++){
+            if(rotacion==0){
+                if (coordenada_x==x && coordenada_y>y-1 || coordenada_y<y+4)
+                {
+                    palo[coordenada_y][coordenada_x]=1;
+                }
+                else{
+                    palo[coordenada_y][coordenada_x]=0;
+                }
+            }
+            else{
+                if (coordenada_y==y && coordenada_x>x-1 || coordenada_x<x+4)
+                {
+                    palo[coordenada_y][coordenada_x]=1;
+                }
+                else{
+                    palo[coordenada_y][coordenada_x]=0;
+                }
+                
+            }
+        }
+    }
+    
 }
